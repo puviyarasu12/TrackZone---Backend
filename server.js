b1c31b9fbf5ddeb6fd84c8a0435f5108439ad0bc
puvia <puviyarasu.s2023it@sece.ac.in>
2025-04-18 15:26:54 +0530

    save

--- server.js
+++ server.js
@@ -87,6 +87,13 @@ if (!process.env.MONGO_URI) {
 }
 
 // 🧹 Middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-employee-token');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
